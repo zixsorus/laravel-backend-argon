8b6843d443de6fbcc6f14bc140ec2f825ed9ed52
unknown <touy_elec@hotmail.com>
2020-09-03 08:29:30 +0700

    remove css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,12 +15,12 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
     <!-- Icons -->
     <link href="{{ asset('argon') }}/vendor/nucleo/css/nucleo.css" rel="stylesheet">
-    {{-- <link href="{{ asset('argon') }}/vendor/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet"> --}}
+    <link href="{{ asset('argon') }}/vendor/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
     <!-- Sweetalert2 -->
     <script src="{{ asset('argon') }}\vendor\sweetalert2\dist\sweetalert2.all.js"></script>
     <!-- Argon CSS -->
     <link type="text/css" href="{{ asset('argon') }}/css/argon.css?v=1.0.0" rel="stylesheet">
-    <link type="text/css" href="{{ mix('css/app.css') }}" rel="stylesheet">
+    {{-- <link type="text/css" href="{{ mix('css/app.css') }}" rel="stylesheet"> --}}
     
 </head>
 
